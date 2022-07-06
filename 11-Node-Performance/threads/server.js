6fbc1998a3efc8ce02baf460ea7b6dfb9b1ce11a
Pedro <pedro.jesus@trendsis.com.br>
2022-07-06 15:29:23 -0300

    Section 11 Completed

---
+++ 11-Node-Performance/threads/server.js
@@ -0,0 +1,16 @@
+const { isMainThread, workerData, Worker } = require('worker_threads');
+
+
+if (isMainThread) {
+  console.log('Main thread ' + process.pid);
+  new Worker(__filename, {
+    workerData: [7, 6, 2, 3]
+  });
+  new Worker(__filename, {
+    workerData: [1, 3, 4, 3]
+  });
+} else {
+  console.log('Worker ' + process.pid);
+  console.log(`${workerData} sorted is ${workerData.sort()}`);
+
+}
